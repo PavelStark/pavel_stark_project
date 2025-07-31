b3ca45a55f1f2b9b84a97ce484b56abdb38979aa
PavelStark <132998912+PavelStark@users.noreply.github.com>
2025-07-31 18:42:50 +0200

    Update index.js

--- Project/src/index.js
+++ Project/src/index.js
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import { persistor, store } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
 
-export const ROOT_URL = "http://localhost:3333";
+export const ROOT_URL = "http://localhost:333";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
